59d1bb0b58aed66ad6b0fe7e290a5d3f41393b5b
Filippo Squillace <squillace.filippo@gmail.com>
2018-04-06 09:02:29 +1000

    Fix in case `cmd` is not installed

--- pearl-config/install.sh
+++ pearl-config/install.sh
@@ -1,10 +1,13 @@
 
 function post_install(){
+    # If `cmd` not installed fallback to the default CMD_VARDIR
+    local cmd_vardir=${CMD_VARDIR:-$PEARL_HOME/var/$PEARL_PKGREPONAME/cmd}
+    mkdir -p $cmd_vardir/bin
 
     for file in $PEARL_PKGDIR/cmds/*/*
     do
         filename="$(basename $file)"
-        link_to "$file" "$CMD_VARDIR/bin/$filename"
+        link_to "$file" "$cmd_vardir/bin/$filename"
     done
 
     info "cmd package is required for cmd-extra:"
@@ -16,9 +19,12 @@ function post_update(){
 }
 
 function pre_remove(){
+    # If `cmd` not installed fallback to the default CMD_VARDIR
+    local cmd_vardir=${CMD_VARDIR:-$PEARL_HOME/var/$PEARL_PKGREPONAME/cmd}
+
     for file in $PEARL_PKGDIR/cmds/*/*
     do
         filename="$(basename $file)"
-        unlink_from "$file" "$CMD_VARDIR/bin/$filename"
+        unlink_from "$file" "$cmd_vardir/bin/$filename"
     done
 }
